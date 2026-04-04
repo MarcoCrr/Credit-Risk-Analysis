a54761519a2538731c23c39aa2128db388d1971f
Marco Corrias <marco.corrias@univie.ac.at>
2026-04-04 14:00:58 +0200

    Creating the first .cpp files

---
+++ cpp/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include "data/DataLoader.h"
+#include "models/LogisticRegression.h"
+
+int main() {
+    std::ifstream test("data/processed/lendingclub_X.csv");
+    std::cout << "File exists: " << test.good() << std::endl;
+    Eigen::MatrixXd X = DataLoader::loadFeatures("data/processed/lendingclub_X.csv");
+    Eigen::VectorXd y = DataLoader::loadTarget("data/processed/lendingclub_y.csv");
+
+    LogisticRegression model(X.cols());
+
+    model.train(X, y, 0.01, 1000);
+
+    Eigen::VectorXd preds = model.predict(X);
+
+    std::cout << "Training complete!" << std::endl;
+
+    return 0;
+}
