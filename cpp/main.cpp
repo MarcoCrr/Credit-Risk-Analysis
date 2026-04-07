f6347d240b579eacf5f40ead47a8f4a8feef14b0
Marco Corrias <marco.corrias@univie.ac.at>
2026-04-07 17:34:27 +0200

    Minor fixes

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,13 +9,19 @@ int main() {
     Eigen::MatrixXd X = DataLoader::loadFeatures("data/processed/lendingclub_X.csv");
     Eigen::VectorXd y = DataLoader::loadTarget("data/processed/lendingclub_y.csv");
 
+    auto [X_train, X_test, y_train, y_test] = train_test_split(X, y);
+    
     LogisticRegression model(X.cols());
 
-    model.train(X, y, 0.01, 1000);
+    model.train(X_train, y_train, 0.01, 1000);
 
-    Eigen::VectorXd preds = model.predict(X);
-
+    Eigen::VectorXd preds = model.predict(X_test);
     std::cout << "Training complete!" << std::endl;
+
+    double acc = accuracy(y_test, preds);
+    std::cout << "Test Accuracy: " << acc << std::endl;
+
+    confusion_matrix(y_test, preds);
 
     return 0;
 }
--- cpp/models/LogisticRegression.cpp
+++ cpp/models/LogisticRegression.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "LogisticRegression.h"
 #include <cmath>
 #include <tuple>
